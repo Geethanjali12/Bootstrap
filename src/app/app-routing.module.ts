528fc6a22317d731049f83bc30bc59a3a8fa6f34
Geethanjali12 <geethanjali@sparkouttech.com>
2021-09-15 09:50:40 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasicDetailsComponent } from './basic-details/basic-details.component';
+import { PaymentComponent } from './payment/payment.component';
+import { SuccessScreenComponent } from './success-screen/success-screen.component';
+import { TokenDetailsComponent } from './token-details/token-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:BasicDetailsComponent},
+  {path:'token-details', component:TokenDetailsComponent},
+  {path:'payment', component:PaymentComponent},
+  {path:'submit', component:SuccessScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicDetailsComponent } from './basic-details/basic-details.component';
+import { TokenDetailsComponent } from './token-details/token-details.component';
+import { PaymentComponent } from './payment/payment.component';
+import { SuccessScreenComponent } from './success-screen/success-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicDetailsComponent,
+    TokenDetailsComponent,
+    PaymentComponent,
+    SuccessScreenComponent
   ],
   imports: [
     BrowserModule,
